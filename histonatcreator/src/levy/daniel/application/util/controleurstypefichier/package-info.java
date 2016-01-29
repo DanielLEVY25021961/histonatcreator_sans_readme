081077e27fb50850011afb6a95f9b3fece478fb3
Daniel Lévy <dany.levy@free.fr>
2016-01-29 20:26:33 +0100

    création du package pour le contrôle du type de fichier (texte comme ASCII, musical comme MP3), photo comme jpg, ...)

---
+++ histonatcreator/src/levy/daniel/application/util/controleurstypefichier/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * class package-info :<br/>
+ * Ce package contient des classes chargées 
+ * de déterminer le type d'un fichier 
+ * (fichier texte (ASCII), fichier musical mp3, photo jpg, etc...).<br/>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author dan Lévy
+ * @version 1.0
+ * @since 29 janv. 2016
+ *
+ */
+package levy.daniel.application.util.controleurstypefichier;
