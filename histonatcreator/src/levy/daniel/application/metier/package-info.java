9d547f6103b34fbe9d61e13bd284ec657f8e9ab2
dan <dan@10.77.222.77>
2016-03-31 20:42:10 +0200

    Après fichiers trafic

---
+++ histonatcreator/src/levy/daniel/application/metier/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * class package-info :<br/>
+ * Ce package contient toutes les classes métier.<br/>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author dan Lévy
+ * @version 1.0
+ * @since 23 mars 2016
+ *
+ */
+package levy.daniel.application.metier;
